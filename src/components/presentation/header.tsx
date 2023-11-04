e89bdfba57e99d47ffbeaaa3cf533ce8f2212901
elmer1394 <148495023+elmer1394@users.noreply.github.com>
2023-11-04 19:02:26 -0400

    Add files via upload

---
+++ src/components/presentation/header.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+import ThemeProvider from "../theme-provider";
+import Navbar from "../navbar";
+import {
+  ArrowSmallRightIcon,
+} from "@heroicons/react/24/outline";
+
+export function HeroPresentation() {
+ 
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <header className="h-full w-screen place-items-center bg-white relative px-8 py-8 lg:mb-36">
+        <div className="container mx-auto grid items-center lg:grid-cols-2">
+          <div className="text-center lg:text-left pt-32">
+            <div className="mb-8 inline-flex items-center">
+              <Typography
+                variant="small"
+                className="mr-3 py-0.5 px-3 font-bold text-dark uppercase border-r border-dark"
+              >
+                BIBLIOTECA CARRERA INGENIERIA AGRONOMICA
+              </Typography>
+              <Typography
+                color="dark"
+                variant="small"
+                className="flex items-center font-bold uppercase"
+              >
+                UNSXX
+              </Typography>
+            </div>
+            <Typography
+              variant="h1"
+              color="blue-gray"
+              className="mb-8 lg:mr-32 leading-tight font-black"
+            >
+              Mira nuestros nuevos libros <span className="text-blue">recien actualizados</span>
+            </Typography>
+            <Typography variant="lead" color="blue-gray" className="lg:pr-32">
+              Llearon los nuevos libros de la especialidad de zootecnia y agroambiente buscalo en los ambientes de la biblioteca.
+            </Typography>
+            <div className="mt-12 flex flex-wrap justify-center gap-3 lg:justify-start">
+              <a href="#pricing">
+                
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:flex">
+          <img src="agro.jpg" alt="components" className="absolute -top-10 right-0 w-1/2" />
+        </div>
+      </header>
+    </ThemeProvider>
+  );
+}
+
+export default HeroPresentation;
